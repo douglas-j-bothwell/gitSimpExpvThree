759ee664239bee2062d2001a5bbab7bf11cfea73
Douglas J Bothwell <doug.bothwell.opnet@gmail.com>
2022-06-06 09:14:25 -0700

    update Mon Jun  6 09:12:01 PDT 2022

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'gitSimpExpvThree my-feature-branch says Hello, Docker!'
+    return 'gitSimpExpvThree main Mon Jun  6 09:12:01 PDT 2022 says Hello, Docker!'
